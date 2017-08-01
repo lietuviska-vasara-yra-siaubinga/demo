869d5f3bef64b93a6574294d2cc167bcc7a3823e
Your Name <you@example.com>
2017-08-01 21:10:13 +0300

    init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socket_http_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var listener = new AsynchronousSocketListener();
+
+            listener.StartListening();
+
+        }
+    }
+}
